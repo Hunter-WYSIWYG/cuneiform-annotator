a9d75fde3f98ad32c2ab30bedf68b665c9b309e2
Timo Homburg <timo.homburg@hs-mainz.de>
2021-05-21 21:16:59 +0000

    Update createThumbnails.py

--- createThumbnails.py
+++ createThumbnails.py
@@ -28,6 +28,7 @@ for filename in os.listdir("result"):
             f.write(jsondata[annotation]["target"]["selector"]["value"])
             f.close()
             path=svg2paths2("temp.svg")
+            print(path)
             print(path.bbox())
             continue
         coords=jsondata[annotation]["target"]["selector"]["value"].replace("xywh","").split(",")
