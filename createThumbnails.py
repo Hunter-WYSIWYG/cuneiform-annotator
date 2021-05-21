4501d2137df384e3ee04329e06043322c33a7154
Timo Homburg <timo.homburg@hs-mainz.de>
2021-05-21 16:45:26 +0000

    Update createThumbnails.py

--- createThumbnails.py
+++ createThumbnails.py
@@ -14,21 +14,22 @@ for filename in os.listdir("result"):
     if filename==".gitkeep" or filename.startswith("."):
         continue
     with open("result/"+filename) as json_file:
-        for annotation in json_file:
-            coords=annotation["target"]["selector"]["value"].replace("xywh","").split(",")
-            translit=""
-            for annobj in annotation["body"]:
-                if annoobj["purpose"]=="Transliteration":
-                    translit=annoobj["value"]
-            if translit in translits:
-                translits[translit]=translits[translit]+1
-            else:
-                translits[translit]=1      
-            f=urlopen(imgurls[filename])
-            with Image(file=f) as img:
-                width=img.width
-                height=img.height
-                with img[coords[0]:coords[1], coords[2]:coords[3]] as cropped:
-                    if(not os.path.exists("public/thumbnails/"+translit)):
-                        os.makedirs("public/thumbnails/"+translit)
-                    cropped.save(filename="public/thumbnails/"+translit+"/"+translit+"_"+translits[translit]+".png")
+        jsondata=json.load(json_file)
+    for annotation in jsondata:
+        coords=annotation["target"]["selector"]["value"].replace("xywh","").split(",")
+        translit=""
+        for annobj in annotation["body"]:
+            if annoobj["purpose"]=="Transliteration":
+                translit=annoobj["value"]
+        if translit in translits:
+            translits[translit]=translits[translit]+1
+        else:
+            translits[translit]=1      
+        f=urlopen(imgurls[filename])
+        with Image(file=f) as img:
+            width=img.width
+            height=img.height
+            with img[coords[0]:coords[1], coords[2]:coords[3]] as cropped:
+                if(not os.path.exists("public/thumbnails/"+translit)):
+                    os.makedirs("public/thumbnails/"+translit)
+                cropped.save(filename="public/thumbnails/"+translit+"/"+translit+"_"+translits[translit]+".png")
