8e65406a89f9d5b94c01ca003e50e5e2f335c0b8
Timo Homburg <timo.homburg@hs-mainz.de>
2021-05-25 17:06:54 +0000

    Update createThumbnails.py

--- createThumbnails.py
+++ createThumbnails.py
@@ -11125,7 +11125,7 @@ for filename in os.listdir("result"):
         charclass="other" #str(translit)
         if(str(translit) in cuneifymap):
             print(cuneifymap[str(translit)])
-            charclass=cuneifymap[str(translit)]
+            charclass=ord(cuneifymap[str(translit)])
         outputcsv+=filename[0:filename.rfind("_")]+";"+filename[filename.rfind("_")+1:].replace(".png.json","")+";"
         outputcsv+=str(coords)+";"+translit+"\n"      
         f=urlopen(imgurls[filename])
