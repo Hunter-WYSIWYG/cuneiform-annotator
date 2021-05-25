70ea90217fe10b115a639c18ec38546d5d9d4e7b
Timo Homburg <timo.homburg@hs-mainz.de>
2021-05-25 22:37:08 +0000

    Update createThumbnails.py

--- createThumbnails.py
+++ createThumbnails.py
@@ -14380,7 +14380,7 @@ for filename in os.listdir("result"):
             outputcsv+=filename[0:filename.rfind("_")]+";"+periods[filename[0:filename.rfind("_")]]+";"+filename[filename.rfind("_")+1:].replace(".png.json","")+";"
             outputcsv+=str(coords)+";"+translit+"\n"          
             periodss[periods[per]]=True
-            translitperiods[str(translit)+"_"+periods[per]=True
+            translitperiods[str(translit)+"_"+periods[per]]=True
             arffdataperiods+=str(translit)+"_"+str(translits[translit])+".jpg,"+periods[per]+"\n"
             arffdatasignperiods+=str(translit)+"_"+str(translits[translit])+".jpg,"+str(translit)+"_"+periods[per]+"\n"
         else:
