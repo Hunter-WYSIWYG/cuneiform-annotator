f26609e5f94e1286424d5ba7505973df55211f8d
Timo Homburg <timo.homburg@hs-mainz.de>
2021-05-25 19:15:53 +0000

    Update createThumbnails.py

--- createThumbnails.py
+++ createThumbnails.py
@@ -14355,7 +14355,7 @@ for filename in os.listdir("result"):
             else:
                 charclass="U+"+str(hex(ord(cuneifymap[str(translit)]))).replace("0x","")
         if charclass.upper() in charlistmap and "signName" in charlistmap[charclass.upper()]:
-            charclass=str(charlistmap[charclass.upper()]["signName"]).encode("ascii", "ignore").replace(" ","_")
+            charclass=str(charlistmap[charclass.upper()]["signName"]).replace(" ","_").encode("ascii", "ignore")
         if charclass in translits:
             translits[charclass]=translits[charclass]+1
         else:
