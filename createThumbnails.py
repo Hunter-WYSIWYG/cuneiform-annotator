9af547606c1c593f15a17f07201e571a01c68d40
Timo Homburg <timo.homburg@hs-mainz.de>
2021-06-05 11:56:53 +0000

    Update .gitlab-ci.yml, createThumbnails.py files

--- createThumbnails.py
+++ createThumbnails.py
@@ -14396,11 +14396,13 @@ for filename in os.listdir("result"):
                 with img[int(coords[0]):int(coords[1]),int(coords[2]):int(coords[3])] as cropped:
                     if singlefolder:
                         with cropped.convert('jpg') as converted:
+                            converted.resize(100, 100)
                             converted.save(filename=exportdir+str(translit)+"_"+str(translits[charclass])+"_"+filename.replace(".png","").replace(".json","")+".jpg")
                     else:
                         if(not os.path.exists(exportdir+str(translit))):
                             os.makedirs(exportdir+str(translit))
                         with cropped.convert('jpg') as converted:
+                            converted.resize(100, 100)
                             converted.save(filename=exportdir+str(translit)+"/"+str(translit)+"_"+str(translits[charclass])+"_"+filename.replace(".png","").replace(".json","")+".jpg")
                     if not translit in homepagejson:
                         homepagejson[translit]=[]
