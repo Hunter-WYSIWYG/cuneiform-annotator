147bbc053890ab45f42b306af94175283b76880c
Timo Homburg <timo.homburg@hs-mainz.de>
2021-06-14 18:20:04 +0000

    Update js/hs23D.js, _layouts/default.html files

---
+++ js/hs23D.js
@@ -0,0 +1,4 @@
+var hs23D={
+"HS 1032B_1":"https://heidicon.ub.uni-heidelberg.de/api/v1/objects/uuid/c84682a4-cf39-4a88-8015-49f664801495/file/id/591868/file_version/name/original/",
+"HS_1198b":"https://heidicon.ub.uni-heidelberg.de/eas/partitions/3/0/592000/592202/adce42325842648a219d9f43d7f86da8744a84b1/application/x-ply/HS_1198b_HeiCuBeDa_GigaMesh.ply"
+}
