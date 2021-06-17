d3b97e5a95fe1865d5ad0f07c1b6b21822af91b0
Timo Homburg <timo.homburg@hs-mainz.de>
2021-06-17 20:45:11 +0000

    Update js/presenter.js

--- js/presenter.js
+++ js/presenter.js
@@ -89,7 +89,6 @@ _parseMesh : function (options) {
 		mType     : null,
 		transform : null
 	}, options);
-	console.log(r)
 	r.transform = this._parseTransform(r.transform);
 	if (r.url) { this._objectsToProcess++; this._objectsToLoad++; }
 	return r;
