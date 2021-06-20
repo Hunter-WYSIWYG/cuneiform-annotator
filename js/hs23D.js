5673a83a49655861736d7f3a1af722e4cb1ddc49
committoken <project16599_bot@example.com>
2021-06-20 18:18:09 +0000

    Committed js/hs23D.js

--- js/hs23D.js
+++ js/hs23D.js
@@ -864,7 +864,19 @@ var hs23D={
     "url": "https://heidicon.ub.uni-heidelberg.de/eas/partitions/1/0/577000/577032/36441671c7e15c54f17272c7c605d4077b1a98bc/application/x-ply/HS_1111_HeiCuBeDa_GigaMesh.ply"
   },
   "HS_1112": {
-    "url": "https://heidicon.ub.uni-heidelberg.de/eas/partitions/3/0/577000/577040/d323ba1bcb68abf4cf93c92c098131f13b6268af/application/x-ply/HS_1112_HeiCuBeDa_GigaMesh.ply"
+    "url": "https://heidicon.ub.uni-heidelberg.de/eas/partitions/3/0/577000/577040/d323ba1bcb68abf4cf93c92c098131f13b6268af/application/x-ply/HS_1112_HeiCuBeDa_GigaMesh.ply",
+    "bbox": {
+      "min": [
+        -20.332273483276367,
+        0,
+        -8.205557823181152
+      ],
+      "max": [
+        20.332273483276367,
+        45.87007141113281,
+        8.205557823181152
+      ]
+    }
   },
   "HS_1113": {
     "url": "https://heidicon.ub.uni-heidelberg.de/eas/partitions/3/0/577000/577049/7e666e648f1b8ffa519974bd9e351b01adaee97a/application/x-ply/HS_1113_HeiCuBeDa_GigaMesh.ply",
