0317a1be2038fc82b266e88f0354f4b0f999f09b
Anja Cramer <anja.cramer@hs-mainz.de>
2021-09-02 13:44:15 +0200

    update

--- pcaexample.py
+++ pcaexample.py
@@ -10,7 +10,8 @@ from plyfile import PlyData
 from sklearn.decomposition import PCA
 from sklearn.datasets import make_classification
 
-input_folder = r"P:\3d-datasets_intern"
+input_folder = r"P:\3d-datasets_intern\Keilschrifttafeln"
+# input_folder = r"E:\temp\Neuer Ordner (7)"
 # meshnames=["H.T._07-31-102_Pulverdruckverfahren_3_Zusammen_mitPuder_mehrPunkte.ply","H.T._07-31-102_SLA_3_Zusammen_mitPuder_mehrPunkte.ply","H.T_07-31-102_FDM_3_Zusammen_mitPuder_mehrPunkte _keine_Nachverarbeitung_mitLoecher.ply", "HT_07-31-47_3D.ply"]
 
 reduce_factors=[1]
@@ -21,6 +22,7 @@ scaling = False
 
 resultfile=input_folder + "/" + "pca_result.txt"
 resultfile_csv=input_folder + "/" + "pca_result.csv"
+logfile = input_folder + "/" + "pca_log.txt"
 
 def rigid_transform_3D(A, B, scale):
     assert len(A) == len(B)
@@ -125,169 +127,175 @@ def do_PCA(mesh):
 
 f = open(resultfile, 'w')  
 c = open(resultfile_csv, 'w')
+l = open(logfile, 'w')
 c.write("meshname | vector-1_length | vector-2_length | vector-3_length | diff v1-v2 mm | diff v1-v2 % v1 | diff v2-v3 mm | diff v2-v3 % v2 | stabil" +"\n")
 
 for root, dirs, files in os.walk (input_folder):
     for meshname in files:
         # print (meshname)
         if os.path.splitext(meshname)[-1]==".ply" or os.path.splitext(meshname)[-1]==".PLY":
-            print (meshname)
-            print("Processing "+str(meshname))
-            plyfile = PlyData.read(root + "/" + meshname)
-            mesh = pd.DataFrame({
-            'x':plyfile['vertex']['x'][::reduce_factor],
-            'y':plyfile['vertex']['y'][::reduce_factor],
-            'z':plyfile['vertex']['z'][::reduce_factor]
-            })
-            pca = do_PCA(mesh)
-          
-            # Objektkoordinaten
-            p0_pca_A = pca[2][0]
-            p1_pca_A = pca[2][1]
-            p2_pca_A = pca[2][2]
-            p3_pca_A = pca[2][3]
-            # pca-Koordinaten
-            p0_pca_B= np.array([0,0,0])
-            p1_pca_B= np.subtract(p1_pca_A, p0_pca_A)
-            p2_pca_B= np.subtract(p2_pca_A, p0_pca_A)
-            p3_pca_B= np.subtract(p3_pca_A, p0_pca_A)
-            # Matrizen
-            A = np.matrix([p0_pca_A , p1_pca_A, p2_pca_A, p3_pca_A])
-            B = np.matrix([p0_pca_B , p1_pca_B, p2_pca_B, p3_pca_B])
+            try:
+                print (meshname)
+                print("Processing "+str(meshname))
+                plyfile = PlyData.read(root + "/" + meshname)
+                mesh = pd.DataFrame({
+                'x':plyfile['vertex']['x'][::reduce_factor],
+                'y':plyfile['vertex']['y'][::reduce_factor],
+                'z':plyfile['vertex']['z'][::reduce_factor]
+                })
+                pca = do_PCA(mesh)
+                
+                # Objektkoordinaten
+                p0_pca_A = pca[2][0]
+                p1_pca_A = pca[2][1]
+                p2_pca_A = pca[2][2]
+                p3_pca_A = pca[2][3]
+                # pca-Koordinaten
+                p0_pca_B= np.array([0,0,0])
+                p1_pca_B= np.subtract(p1_pca_A, p0_pca_A)
+                p2_pca_B= np.subtract(p2_pca_A, p0_pca_A)
+                p3_pca_B= np.subtract(p3_pca_A, p0_pca_A)
+                # Matrizen
+                A = np.matrix([p0_pca_A , p1_pca_A, p2_pca_A, p3_pca_A])
+                B = np.matrix([p0_pca_B , p1_pca_B, p2_pca_B, p3_pca_B])
 
-            # 10% der Länge
-            dist_v1 = np.linalg.norm(p0_pca_A - p1_pca_A)
-            dist_v2 = np.linalg.norm(p0_pca_A - p2_pca_A)
-            dist_v3 = np.linalg.norm(p0_pca_A - p3_pca_A)
-            print (dist_v1)
-            print (dist_v2)
-            print (dist_v3)
+                # 10% der Länge
+                dist_v1 = np.linalg.norm(p0_pca_A - p1_pca_A)
+                dist_v2 = np.linalg.norm(p0_pca_A - p2_pca_A)
+                dist_v3 = np.linalg.norm(p0_pca_A - p3_pca_A)
+                print (dist_v1)
+                print (dist_v2)
+                print (dist_v3)
 
-            # differenzen
-            diff_v1_v2 = dist_v1 - dist_v2
-            diff_v2_v3 = dist_v2 - dist_v3
-            diff_v1_v2_p = 100 * (dist_v1 - dist_v2) / dist_v1
-            diff_v2_v3_p = 100 * (dist_v2 - dist_v3) / dist_v2
+                # differenzen
+                diff_v1_v2 = dist_v1 - dist_v2
+                diff_v2_v3 = dist_v2 - dist_v3
+                diff_v1_v2_p = 100 * (dist_v1 - dist_v2) / dist_v1
+                diff_v2_v3_p = 100 * (dist_v2 - dist_v3) / dist_v2
 
-            # stabil ???
-            if diff_v1_v2 > dist_v1/10 and diff_v2_v3 > dist_v2/10:
-                stabil = True
-            else:
-                stabil = False
+                # stabil ???
+                if diff_v1_v2 > dist_v1/10 and diff_v2_v3 > dist_v2/10:
+                    stabil = True
+                else:
+                    stabil = False
 
-            f.write(" 10 %" +"\n")
-            f.write(" vector 1: " + str(dist_v1) + " 10% " + str(dist_v1/10) +"\n")
-            f.write(" vector 2: " + str(dist_v2) + " 10% " + str(dist_v2/10) +"\n")
-            f.write(" vector 3: " + str(dist_v3) + " 10% " + str(dist_v3/10) +"\n")
+                f.write(" 10 %" +"\n")
+                f.write(" vector 1: " + str(dist_v1) + " 10% " + str(dist_v1/10) +"\n")
+                f.write(" vector 2: " + str(dist_v2) + " 10% " + str(dist_v2/10) +"\n")
+                f.write(" vector 3: " + str(dist_v3) + " 10% " + str(dist_v3/10) +"\n")
 
-            f.write(" differenzen" +"\n")
-            f.write(" diff v1-v2: " + str(round((diff_v1_v2),1)) + " mm = " + str(round((diff_v1_v2_p),1)) + "% von vector 1 \n")
-            f.write(" diff v2-v3: " + str(round((diff_v2_v3),1)) + " mm = " + str(round((diff_v2_v3_p),1)) + "% von vector 2 \n")
+                f.write(" differenzen" +"\n")
+                f.write(" diff v1-v2: " + str(round((diff_v1_v2),1)) + " mm = " + str(round((diff_v1_v2_p),1)) + "% von vector 1 \n")
+                f.write(" diff v2-v3: " + str(round((diff_v2_v3),1)) + " mm = " + str(round((diff_v2_v3_p),1)) + "% von vector 2 \n")
 
-            f.write("\n" + "--->>> pca stabil: " + str(stabil) + "\n" + "\n" )
+                f.write("\n" + "--->>> pca stabil: " + str(stabil) + "\n" + "\n" )
 
-            c.write(meshname + "|" + str(round((dist_v1),2)) + "|" + str(round((dist_v2),2)) + "|" + str(round((dist_v3),2)) + "|" + str(round((diff_v1_v2),2)) + "|" + str(round((diff_v1_v2_p),2)) + "|" + str(round((diff_v2_v3),2)) + "|" + str(round((diff_v2_v3_p),2)) + "|" + str(stabil) + "\n")
-
-
-
-            # f.write("Matrix A" +"\n")
-            # f.write(str(A)+"\n")
-            # f.write("Matrix B" +"\n")
-            # f.write(str(B)+"\n")
-
-            # print ("Matix A")
-            # print (A)
-            # print("")
-            # print ("Matix B")
-            # print (B)
-            # print("")
-
-            s, ret_R, ret_t=rigid_transform_3D(A, B,False)
-
-            
-            n = B.shape[0]  	    
-
-            ## Find the error
-            B2 = (ret_R * B.T) + np.tile(ret_t, (1, n))
-            B2 = B2.T
-            err = A - B2
-            err = np.multiply(err, err)
-            err = np.sum(err)
-            rmse = sqrt(err / n);
-
-            ##convert to 4x4 transform
-            pca2obj = np.zeros((4,4))
-            pca2obj[:3,:3] = ret_R
-            pca2obj[0,3] = ret_t[0]
-            pca2obj[1,3] = ret_t[1]
-            pca2obj[2,3] = ret_t[2]
-            pca2obj[3,3] = 1
-
-            obj2pca = inv(np.matrix(pca2obj))
-
-            # print "Points A"
-            # print A
-            # print ""
-
-            # print "Points B"
-            # print B
-            # print ""
-
-            # print "Rotation"
-            # print ret_R
-            # print ""
-
-            # print "Translation"
-            # print ret_t
-            # print ""
-
-            # print "Scale"
-            # print s
-            # print ""
+                c.write(meshname + "|" + str(round((dist_v1),2)) + "|" + str(round((dist_v2),2)) + "|" + str(round((dist_v3),2)) + "|" + str(round((diff_v1_v2),2)) + "|" + str(round((diff_v1_v2_p),2)) + "|" + str(round((diff_v2_v3),2)) + "|" + str(round((diff_v2_v3_p),2)) + "|" + str(stabil) + "\n")
 
 
 
-            # f.write("Rotation" +"\n")
-            # f.write(str(ret_R)+"\n")
-            # f.write("Translation" +"\n")
-            # f.write(str(ret_t)+"\n")
-            # f.write("Scale" +"\n")
-            # f.write(str(s)+"\n")
-            # f.write("Homogeneous Transform pca2obj" +"\n")
-            # f.write(str(pca2obj)+"\n")
-            # f.write("Homogeneous Transform obj2pca" +"\n")
-            # f.write(str(obj2pca)+"\n")
-            # f.write("RMSE" +"\n")
-            # f.write(str(rmse)+"\n")
+                # f.write("Matrix A" +"\n")
+                # f.write(str(A)+"\n")
+                # f.write("Matrix B" +"\n")
+                # f.write(str(B)+"\n")
 
-            print("Rotation")
-            print(ret_R)
-            print("")
+                # print ("Matix A")
+                # print (A)
+                # print("")
+                # print ("Matix B")
+                # print (B)
+                # print("")
 
-            print("Translation") 
-            print(ret_t)
-            print("")
+                s, ret_R, ret_t=rigid_transform_3D(A, B,False)
 
-            print("Scale")
-            print(s)
-            print("")
+                
+                n = B.shape[0]  	    
 
-            print ("Homogeneous Transform pca2obj")
-            print (pca2obj)
-            print ("")
+                ## Find the error
+                B2 = (ret_R * B.T) + np.tile(ret_t, (1, n))
+                B2 = B2.T
+                err = A - B2
+                err = np.multiply(err, err)
+                err = np.sum(err)
+                rmse = sqrt(err / n);
 
-            # if scaling:
-            #     print ("Total Diff to SA matrix")
-            #     print (np.sum(match_target - Tstarg))
-            #     print ("")
-            # else:
-            #     print ("Total Diff to SA matrix")
-            #     print (np.sum(match_target - Ttarg))
-            #     print ("")
+                ##convert to 4x4 transform
+                pca2obj = np.zeros((4,4))
+                pca2obj[:3,:3] = ret_R
+                pca2obj[0,3] = ret_t[0]
+                pca2obj[1,3] = ret_t[1]
+                pca2obj[2,3] = ret_t[2]
+                pca2obj[3,3] = 1
 
-            print ("RMSE:" +  str(rmse))
-            print ("If RMSE is near zero, the function is correct!")
+                obj2pca = inv(np.matrix(pca2obj))
+
+                # print "Points A"
+                # print A
+                # print ""
+
+                # print "Points B"
+                # print B
+                # print ""
+
+                # print "Rotation"
+                # print ret_R
+                # print ""
+
+                # print "Translation"
+                # print ret_t
+                # print ""
+
+                # print "Scale"
+                # print s
+                # print ""
+
+
+
+                # f.write("Rotation" +"\n")
+                # f.write(str(ret_R)+"\n")
+                # f.write("Translation" +"\n")
+                # f.write(str(ret_t)+"\n")
+                # f.write("Scale" +"\n")
+                # f.write(str(s)+"\n")
+                # f.write("Homogeneous Transform pca2obj" +"\n")
+                # f.write(str(pca2obj)+"\n")
+                # f.write("Homogeneous Transform obj2pca" +"\n")
+                # f.write(str(obj2pca)+"\n")
+                # f.write("RMSE" +"\n")
+                # f.write(str(rmse)+"\n")
+
+                print("Rotation")
+                print(ret_R)
+                print("")
+
+                print("Translation") 
+                print(ret_t)
+                print("")
+
+                print("Scale")
+                print(s)
+                print("")
+
+                print ("Homogeneous Transform pca2obj")
+                print (pca2obj)
+                print ("")
+
+                # if scaling:
+                #     print ("Total Diff to SA matrix")
+                #     print (np.sum(match_target - Tstarg))
+                #     print ("")
+                # else:
+                #     print ("Total Diff to SA matrix")
+                #     print (np.sum(match_target - Ttarg))
+                #     print ("")
+
+                print ("RMSE:" +  str(rmse))
+                print ("If RMSE is near zero, the function is correct!")
+            except:
+                l.write(meshname + "\n")
+
 f.close()
 c.close()
+l.close()
 
 print ("fertsch")
