ebf24c609d70fdddd17a2f385c5b12a84de75b44
Timo Homburg <timohomburg@c-gv-c3po.intern.hs-mainz.de>
2021-09-11 13:15:31 +0200

    update

--- createThumbnails.py
+++ createThumbnails.py
@@ -686,15 +686,15 @@ if singlefolder:
     sorte_periodss = sorted(periodss.items(), key=lambda x: x[1])
     sort_periodss={}
     for so in sorte_periodss:
-        sort_periodss[so]=sorte_periodss[so]  
+        sort_periodss[sorte_periodss[so][0]]=sorte_periodss[so][1]  
     sorte_genress = sorted(genress.items(), key=lambda x: x[1])
     sort_genress={}
     for so in sorte_genress:
-        sort_genress[so]=sorte_genress[so]  
+        sort_genress[sorte_genress[so][0]]=sorte_genress[so][1]  
     sort_languagess = sorted(languagess.items(), key=lambda x: x[1])
     sort_languagess={}
     for so in sorte_languagess:
-        sort_languagess[so]=sorte_languagess[so]  
+        sort_languagess[sorte_languagess[so][0]]=sorte_languagess[so][1]  
     f = open(exportdir+"/homepagestats.js", 'w')   
     f.write("var charperperiod="+json.dumps(sort_charperiods,indent=2)+";\n"+"var acc_charperperiod="+json.dumps(acc_charperiods,indent=2)+";\nvar numberCharPeriods="+json.dumps(sort_periodss,indent=2)+";\nvar numberLanguages="+json.dumps(sort_languagess,indent=2)+";\nvar genres="+json.dumps(sort_genress,indent=2))
     f.close()
@@ -770,15 +770,15 @@ else:
     sorte_periodss = sorted(periodss.items(), key=lambda x: x[1])
     sort_periodss={}
     for so in sorte_periodss:
-        sort_periodss[so]=sorte_periodss[so]  
+        sort_periodss[sorte_periodss[so][0]]=sorte_periodss[so][1]  
     sorte_genress = sorted(genress.items(), key=lambda x: x[1])
     sort_genress={}
     for so in sorte_genress:
-        sort_genress[so]=sorte_genress[so]  
+        sort_genress[sorte_genress[so][0]]=sorte_genress[so][1]  
     sort_languagess = sorted(languagess.items(), key=lambda x: x[1])
     sort_languagess={}
     for so in sorte_languagess:
-        sort_languagess[so]=sorte_languagess[so]  
+        sort_languagess[sorte_languagess[so][0]]=sorte_languagess[so][1]  
     f = open(exportdir+"/homepagestats.js", 'w')   
     f.write("var charperperiod="+json.dumps(sort_charperiods,indent=2)+";\n"+"var acc_charperperiod="+json.dumps(acc_charperiods,indent=2)+";\nvar numberCharPeriods="+json.dumps(sort_periodss,indent=2)+";\nvar numberLanguages="+json.dumps(sort_languagess,indent=2)+";\nvar genres="+json.dumps(sort_genress,indent=2))
     f.close()
