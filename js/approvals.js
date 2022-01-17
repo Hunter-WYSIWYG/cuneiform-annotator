bd02c95fd98e5e73f982cce289ee3d62bc1895d2
committoken <project16599_bot@example.com>
2022-01-17 17:05:34 +0100

    Committed approvals for HS_0044_03_front.png.json

--- js/approvals.js
+++ js/approvals.js
@@ -1,1 +1,11 @@
-var approvals={}
+{
+  "HS_0044": {
+    "03_front": {
+      "positioningcorrect": "unknown",
+      "transliterationcorrect": "unknown",
+      "indexingcorrect": "unknown",
+      "annotationscorrect": "unknown",
+      "annotationscomplete": "unknown"
+    }
+  }
+}
