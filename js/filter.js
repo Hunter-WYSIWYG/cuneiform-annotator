9ea0f42740c84441b0700edf08abe44dc0d147af
Hunter-WYSIWYG <hunter23798@gmail.com>
2022-03-03 17:01:28 +0100

    universal filter complete

--- js/filter.js
+++ js/filter.js
@@ -95,6 +95,14 @@ function reduceToDistinctField(obj, field) {
   ).sort();
 }
 
+/**
+ * an array of every supported filter category name
+ */
+var filterCategories = ["period","language","provenience","genre","material"]
+
+/**
+ * init arrays of distinct filter values
+ */
 var distinctPeriods =
   sortByPeriod(
     removeEmpty(
@@ -103,42 +111,120 @@ var distinctPeriods =
       )
     )
   );
-var distinctLanguages = reduceToDistinctField(languages, 'language');
-var distinctProvenience = reduceToDistinctField(languages, 'provenience');
-var distinctGenre = reduceToDistinctField(languages, 'genre');
-var distinctMaterial = reduceToDistinctField(languages, 'material');
-var filterCategories = ["period","language","provenience","genre","material"]
+var distinctLanguages = reduceToDistinctField(languages, filterCategories[1]);
+var distinctProvenience = reduceToDistinctField(languages, filterCategories[2]);
+var distinctGenre = reduceToDistinctField(languages, filterCategories[3]);
+var distinctMaterial = reduceToDistinctField(languages, filterCategories[4]);
 
-/**
- * returns an array of all period tuples that match the period filter
- */
- function filteredPeriodTuples(periodFilter) {
-    var periodsArray = Object.entries(periods);
-    return periodsArray.filter(function(periodTuple) {
-        return periodTuple[1] == periodFilter;
-    })
-}
+//activeFilter = [[periods],[languages],...]
 
 /**
  * concat the first element of every array in an array of arrays into a new array
  * example: [[1,2],[4,5],[7,8]] -> [1,4,7]
  */
-function concatFirst(arr) {
+ function concatFirst(arr) {
     return arr.map(function(item) {
         return item[0];
     });
 }
 
 /**
- * apply the current filter to the current urls
+ * returns an array of tablet names that match the given period filter
  */
-function filterSuggestions(currentPeriodFilter, currentLanguageFilter) {
-    var filteredTabletNames = concatFirst(filteredPeriodTuples(currentPeriodFilter));
+function filterPeriodTuples(periodName) {
+    let periodsArray = Object.entries(periods);
+    let periodTuples = periodsArray.filter(function(periodTuple) {
+        return periodTuple[1] == periodName;
+    })
+    return concatFirst(periodTuples);
+}
+
+/**
+ * return an array of tablet names that fit every period filter
+ */
+function filterPeriodTabletNames(periodNames) {
+    let result = [];
+    for (currentIndex in periodNames) {
+        result = result.concat(filterPeriodTuples(periodNames[currentIndex]));
+    }
+    return eliminateDuplicates(result);
+}
+
+/**
+ * returns an array of tablet names that match the given filter of this category for language objects
+ */
+function filterLanguageObjects(category, categoryfilterName) {
+    var languageArray = Object.entries(languages);
+    let languageTuples = languageArray.filter(function(languageTuple) {
+        let languageObject = languageTuple[1]
+        return languageObject[category] == categoryfilterName;
+    })
+    return concatFirst(languageTuples);
+}
+
+/**
+ * returns an array of tablet names that match every filter of this category for language objects
+ */
+function filterLanguageTabletNames(category, categoryNames) {
+    let result = [];
+    for (currentIndex in categoryNames) {
+        result = result.concat(filterLanguageObjects(category, categoryNames[currentIndex]));
+    }
+    return eliminateDuplicates(result);
+}
+
+/**
+ * returns the intersection of 2 arrays
+ */
+function intersect(arr1, arr2) {
+    return arr1.filter(function(item) {
+        return arr2.indexOf(item) !== -1;
+    });
+}
+
+/**
+ * returns the union of 2 arrays
+ */
+ function union(arr1, arr2) {
+    return eliminateDuplicates(arr1.concat(arr2));
+}
+
+/**
+ * returns an array of all tablet names that should be visible with the current filters
+ */
+function getfilteredTabletNames() {
+    let filteredTabletNames = concatFirst(Object.entries(urls));
+    let noActivefilters = true;
+    for (index in activeFilter) {
+        noActivefilters = noActivefilters && (activeFilter[index].length <= 0)
+    }
+    if (!noActivefilters) {
+        let activeTablets = [];
+        for (categoryIndex in filterCategories) {
+            let category = filterCategories[categoryIndex]
+            if (category == "period") {
+                activeTablets = union(activeTablets, filterPeriodTabletNames(activeFilter[categoryIndex]));
+            } else {
+                activeTablets = union(activeTablets, filterLanguageTabletNames(category, activeFilter[categoryIndex]));
+            }
+        }
+        filteredTabletNames = intersect(filteredTabletNames, activeTablets);
+    }
+    console.log(filteredTabletNames);
+    return filteredTabletNames;
+}
+
+
+/**
+ * apply the current filter to the urls
+ */
+function filterSuggestions() {
+    var filteredTabletNames = getfilteredTabletNames();
 
     var filtered2DUrls = Object.entries(urls).filter(function(urlArray) {
         return (filteredTabletNames.includes(urlArray[0]));
     });
-    currentUrls = Object.fromEntries(filtered2DUrls);
+    current2DUrls = Object.fromEntries(filtered2DUrls);
 
     var filtered3DUrls = Object.entries(hs23D).filter(function(urlArray) {
         return (filteredTabletNames.includes(urlArray[0]));
